97beca87e4036e540033732464cf822ef123f539
aboongm <aboongm@yahoo.com>
2023-11-13 16:58:02 +0530

    Integrate W3Mwagmi template

---
+++ ui-mobile/src/App.tsx
@@ -0,0 +1,131 @@
+import '@walletconnect/react-native-compat';
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+import {
+  createWeb3Modal,
+  defaultWagmiConfig,
+  Web3Modal,
+  W3mButton,
+} from '@web3modal/wagmi-react-native';
+import {FlexView, Text} from '@web3modal/ui-react-native';
+import Clipboard from '@react-native-clipboard/clipboard';
+import * as Sentry from '@sentry/react-native';
+
+import {WagmiConfig} from 'wagmi';
+import {
+  arbitrum,
+  mainnet,
+  polygon,
+  avalanche,
+  bsc,
+  optimism,
+  gnosis,
+  zkSync,
+  zora,
+  base,
+  celo,
+  aurora,
+} from 'wagmi/chains';
+import {ENV_PROJECT_ID, ENV_SENTRY_DSN} from '@env';
+import {SignMessage} from './views/SignMessage';
+import {SendTransaction} from './views/SendTransaction';
+import {ReadContract} from './views/ReadContract';
+
+if (!__DEV__ && ENV_SENTRY_DSN) {
+  Sentry.init({
+    dsn: ENV_SENTRY_DSN,
+  });
+}
+
+// 1. Get projectId
+const projectId = "778ab3a036060b46ad14bab5c834c5a0";
+
+// 2. Create config
+const metadata = {
+  name: 'Web3Modal + wagmi',
+  description: 'Web3Modal + wagmi',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886'],
+  redirect: {
+    native: 'w3mwagmisample://',
+  },
+};
+
+const clipboardClient = {
+  setString: async (value: string) => {
+    Clipboard.setString(value);
+  },
+};
+
+const chains = [
+  mainnet,
+  polygon,
+  avalanche,
+  arbitrum,
+  bsc,
+  optimism,
+  gnosis,
+  zkSync,
+  zora,
+  base,
+  celo,
+  aurora,
+];
+
+const wagmiConfig = defaultWagmiConfig({chains, projectId, metadata});
+
+// 3. Create modal
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig,
+  clipboardClient,
+});
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <SafeAreaView style={[styles.container, isDarkMode && styles.dark]}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <Text style={styles.title} variant="large-600">
+          Web3Modal + wagmi + me
+        </Text>
+        <FlexView style={styles.buttonContainer}>
+          <W3mButton balance="show" />
+          <SignMessage />
+          <SendTransaction />
+          <ReadContract />
+        </FlexView>
+        <Web3Modal />
+      </SafeAreaView>
+    </WagmiConfig>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+  },
+  buttonContainer: {
+    gap: 4,
+  },
+  dark: {
+    backgroundColor: '#141414',
+  },
+  title: {
+    marginBottom: 40,
+    fontSize: 30,
+  },
+});
+
+export default App;
