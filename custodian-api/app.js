0adc90812c0be9589bf2ecf7ffdc063eb471264e
aboongm <aboongm@yahoo.com>
2023-11-16 15:35:44 +0530

    update

--- custodian-api/app.js
+++ custodian-api/app.js
@@ -6,7 +6,6 @@ const app = express();
 require("dotenv").config();
 const api = process.env.API_URL;
 
-
 app.use(cors());
 app.options("*", cors());
 
@@ -14,17 +13,10 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-    res.send("10000");
+    res.json({ "goldReserves": 10000 }); 
 });
 
-// Routes
-// const loanRoutes = require("./routes/loansRoutes");
-// const userRoutes = require("./routes/userRoutes");
 
-// app.use(`${api}/loans`, loanRoutes); 
-// app.use(`${api}`, userRoutes); 
-
-// Production
 let server = app.listen(process.env.PORT || 3000, function () {
   let port = server.address().port;
   console.log("Express is working on port " + port);
