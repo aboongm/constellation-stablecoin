e38790ca225bbcd7e18e5e07e1e2b7e963f83f37
aboongm <aboongm@yahoo.com>
2023-11-18 11:17:50 +0530

    Update the webapp initial setup to include rainbowkit

--- ui-web/src/main.tsx
+++ ui-web/src/main.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './polyfills';
+import './global.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { sepolia, mainnet, polygon, optimism, arbitrum, base, zora } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const { chains, publicClient } = configureChains(
+  [sepolia, mainnet, polygon, optimism, arbitrum, base, zora],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'RainbowKit demo',
+  projectId: '778ab3a036060b46ad14bab5c834c5a0',
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
