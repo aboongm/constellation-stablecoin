a7538203f76c9f0cf2f7a8ef5141c54195e69102
aboongm <aboongm@yahoo.com>
2023-12-01 16:09:58 +0530

    Refactor to CoinDollarupdate environment variable

--- scripts/deployCoinDollar.ts
+++ scripts/deployCoinDollar.ts
@@ -1,26 +1,41 @@
 import { ethers } from 'hardhat';
+import fs from 'fs'
+import { promisify} from 'util'
+import dotenv from "dotenv"
+dotenv.config();
 
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
 
-  // Example parameters for the CoinGold constructor
   const name = "CoinDollar";
   const symbol = "CNDO";
-  const _coinGoldAddress = "0x5DAEeD2Ba702fb7eB0b863e6bA3D6A1D95f3Fe6a";
+  const _coinGoldAddress = process.env.COINGOLD_ADDRESS || "0x712D272A886dCa26D712C274E4b32179e80F5B54";
   const _dataFeedAddress = "0xC5981F461d74c46eB4b0CF3f4Ec79f025573B0Ea";
-  // Deploy the CoinGold contract with the specified parameters
+  
   const CoinDollar = await ethers.getContractFactory("CoinDollar");
   const token = await CoinDollar.deploy(name, symbol, _coinGoldAddress, _dataFeedAddress);
 
-  // Get the contract address from the deployed contract instance
   const contractAddress = await token.getAddress();
-  console.log("Token address:", contractAddress);
-  // deployed CoinDollar Address: 0xAb23dEA7210242778953C5FF31FED4b98E526EB7
+  console.log("CoinDollar address:", contractAddress);
+
+  let addresses = [
+    `COINDOLLAR_ADDRESS=${await token.getAddress()}`,
+  ]
+  const data = '\n' + addresses.join('\n')
+
+  const writeFile = promisify(fs.appendFile);
+  const filePath = '.env';
+  return writeFile(filePath, data)
+      .then(() => {
+        console.log('Addresses recorded.');
+      })
+      .catch((error) => {
+        console.error('Error logging addresses:', error);
+        throw error;
+      });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
