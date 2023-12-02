35874018a1ceedef9acd4dcc5363f9d5e8354bd6
aboongm <aboongm@yahoo.com>
2023-12-02 20:58:48 +0530

    Balance updated

--- ui-web/src/components/Balances.tsx
+++ ui-web/src/components/Balances.tsx
@@ -18,7 +18,7 @@ export default function Balances() {
     const [expandedAddress, setExpandedAddress] = useState<string | null>(null);
     const coinGoldToken = useToken({ address: tokenContracts.CoinGold });
     const coinDollarToken = useToken({ address: tokenContracts.CoinDollar });
-    
+
     const renderAddress = (address: string, token: string) => {
         if (!address) return '';
         return expandedAddress === token ? address : `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
@@ -34,40 +34,62 @@ export default function Balances() {
             try {
                 const supply: { [key: string]: string } = {};
 
-                if (coinGoldToken.isSuccess) {
+                if (coinGoldToken.isSuccess && coinGoldToken.data && coinGoldToken.data.totalSupply) {
                     const formattedCoinGoldSupply = parseFloat(coinGoldToken.data.totalSupply.formatted).toFixed(4);
                     supply.CoinGold = formattedCoinGoldSupply;
                 }
 
-                if (coinDollarToken.isSuccess) {
+                if (coinDollarToken.isSuccess && coinDollarToken.data && coinDollarToken.data.totalSupply) {
                     const formattedCoinDollarSupply = parseFloat(coinDollarToken.data.totalSupply.formatted).toFixed(4);
                     supply.CoinDollar = formattedCoinDollarSupply;
                 }
 
-                const balanceCoinGold = await publicClient.readContract({
-                    address: tokenContracts.CoinGold,
-                    abi: abiCoinGold.abi,
-                    functionName: 'balanceOf',
-                    args: [accountAddress]
-                })
+                let balanceCoinGold: bigint;
 
-                const balanceCoinDollar = await publicClient.readContract({
-                    address: tokenContracts.CoinDollar,
-                    abi: abiCoinDollar.abi,
-                    functionName: 'balanceOf',
-                    args: [accountAddress]
-                })
-                console.log("CoinGold: ", balanceCoinGold);
-                console.log("CoinDollar: ", balanceCoinDollar);
-                
+                try {
+                    const result = await publicClient.readContract({
+                        address: tokenContracts.CoinGold,
+                        abi: abiCoinGold.abi,
+                        functionName: 'balanceOf',
+                        args: [accountAddress]
+                    });
 
-                if (balanceCoinGold) {
-                    const humanReadableCoinGoldBalance = (parseFloat(balanceCoinGold) / 1e18).toFixed(4)
-                    setCoinGoldBalance(humanReadableCoinGoldBalance.toString());
+                    if (typeof result === 'bigint') {
+                        balanceCoinGold = result;
+                    } else if (typeof result === 'string') {
+                        balanceCoinGold = BigInt(result);
+                    } else {
+                        throw new Error('Unexpected return type');
+                    }
+
+                    const humanReadableCoinGoldBalance = (Number(balanceCoinGold) / 1e18).toFixed(4)
+                    setCoinGoldBalance(humanReadableCoinGoldBalance);
+                } catch (error) {
+                    console.error('Error fetching balanceCoinGold:', error);
                 }
-                if (balanceCoinDollar) {
-                    const humanReadableCoinDollarBalance = (parseFloat(balanceCoinDollar) / 1e18).toFixed(4)
-                    setCoinDollarBalance(humanReadableCoinDollarBalance.toString())
+
+                let balanceCoinDollar: bigint;
+
+                try {
+                    const result = await publicClient.readContract({
+                        address: tokenContracts.CoinDollar,
+                        abi: abiCoinDollar.abi,
+                        functionName: 'balanceOf',
+                        args: [accountAddress]
+                    });
+
+                    if (typeof result === 'bigint') {
+                        balanceCoinDollar = result;
+                    } else if (typeof result === 'string') {
+                        balanceCoinDollar = BigInt(result);
+                    } else {
+                        throw new Error('Unexpected return type');
+                    }
+
+                    const humanReadableCoinDollarBalance = (Number(balanceCoinDollar) / 1e18).toFixed(4)
+                    setCoinDollarBalance(humanReadableCoinDollarBalance);
+                } catch (error) {
+                    console.error('Error fetching balanceCoinDollar:', error);
                 }
 
                 setTokenSupply(supply);
@@ -86,42 +108,42 @@ export default function Balances() {
     return (
         <div style={{ display: 'flex', minWidth: '320px', margin: '0 auto', flexDirection: 'column', alignItems: 'center' }}>
             <div>
-                <h3 style={{ marginLeft: "1rem", color: '#5f5959'}}>CoinGold</h3>
+                <h3 style={{ marginLeft: "1rem", color: '#5f5959' }}>CoinGold</h3>
                 <div className={styles.card}>
-                    <div style={{display: 'flex', gap: 10}}>
+                    <div style={{ display: 'flex', gap: 10 }}>
                         <p>Address:</p>
                         <button onClick={() => toggleExpandedAddress('CoinGold')} style={{ borderRadius: 4, borderColor: 'lightgrey' }}>
                             <p >{renderAddress(tokenContracts.CoinGold, 'CoinGold')}</p>
                         </button>
                     </div>
-                    <div style={{display: 'flex', gap: 10}}>
+                    <div style={{ display: 'flex', gap: 10 }}>
                         <p>TotalSupply:</p>
                         <p>{tokenSupply.CoinGold}</p>
                         <p>CNGD</p>
                     </div>
-                    <div style={{display: 'flex', gap: 10}}>
+                    <div style={{ display: 'flex', gap: 10 }}>
                         <p>MyBalance  :</p>
                         <p>{coinGoldBalance}</p>
                         <p>CNGD</p>
                     </div>
                 </div>
             </div>
-            
+
             <div>
-                <h3 style={{ marginLeft: "1rem", color: '#5f5959'}}>CoinDollar</h3>
+                <h3 style={{ marginLeft: "1rem", color: '#5f5959' }}>CoinDollar</h3>
                 <div className={styles.card}>
-                    <div style={{display: 'flex', gap: 10}}>
+                    <div style={{ display: 'flex', gap: 10 }}>
                         <p>Address:</p>
                         <button onClick={() => toggleExpandedAddress('CoinDollar')} style={{ borderRadius: 4, borderColor: 'lightgrey' }}>
                             <p >{renderAddress(tokenContracts.CoinDollar, 'CoinDollar')}</p>
                         </button>
                     </div>
-                    <div style={{display: 'flex', gap: 10}}>
+                    <div style={{ display: 'flex', gap: 10 }}>
                         <p>TotalSupply:</p>
                         <p>{tokenSupply.CoinDollar}</p>
                         <p>CNDO</p>
                     </div>
-                    <div style={{display: 'flex', gap: 10}}>
+                    <div style={{ display: 'flex', gap: 10 }}>
                         <p>MyBalance :</p>
                         <p>{coinDollarBalance}</p>
                         <p>CNDO</p>
