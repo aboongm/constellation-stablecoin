c5ce736c87c560d859be74483ddd4f2241bc2327
aboongm <aboongm@yahoo.com>
2023-12-02 16:16:28 +0530

    Refactor and remove comments

--- ui-web/src/utils/TransferCoinDollar.tsx
+++ ui-web/src/utils/TransferCoinDollar.tsx
@@ -1,18 +1,14 @@
 import { ethers } from "ethers";
-// import abiCoinDollar from "../../abi/abi-CoinDollar.json";
 import abiCoinDollar from "../../../artifacts/contracts/CoinDollar.sol/CoinDollar.json"
 
-// Contract address and ABI of the token
-const tokenAddress = import.meta.env.VITE_COINDOLLAR_ADDRESS; // Replace with the actual token contract address
-const tokenAbi = abiCoinDollar.abi; // Replace with the actual token ABI
+const tokenAddress = import.meta.env.VITE_COINDOLLAR_ADDRESS;
+const tokenAbi = abiCoinDollar.abi; 
 
 const provider = new ethers.BrowserProvider(window.ethereum);
   
-  // It will prompt user for account connections if it isnt connected
   const signer = await provider.getSigner();
   console.log("Account:", await signer.getAddress());
   
-  // The contract instance of the token using the signer
   const tokenContract = new ethers.Contract(tokenAddress, tokenAbi, signer);
 
 export async function TransferCoinDollar(recipientAddress: string, amount: string, isConnected: boolean) {
@@ -27,27 +23,22 @@ export async function TransferCoinDollar(recipientAddress: string, amount: strin
             alert("Insufficient balance for the transfer.");
         return;
         }
-      // Estimate gas for the transaction
+
       const estimatedGas = await tokenContract.transfer.estimateGas(recipientAddress, amountToSend)
       console.log("estimated gas: ", estimatedGas);
       
   
-      // Display the gas estimate to the user
       const gasPrice = (await provider.getFeeData())?.gasPrice 
       let gasValue;
       if (gasPrice !== null && gasPrice !== undefined) {
         gasValue = ethers.formatEther(gasPrice * estimatedGas);
-        // Use gasValue here or perform further operations
       } else {
-        // Handle the case where gasPrice is null or undefined
         console.error('Gas price is null or undefined');
       }
       
-      // Ask for user confirmation with gas information
       const userConfirmation = window.confirm(`Transaction Details:\nRecipient: ${recipientAddress}\nAmount: ${amount.toString()} COINDOLLAR\nGas Estimate: ${estimatedGas}\nGas Fee: ${gasValue} ETH\n\nConfirm Transaction?`);
   
       if (userConfirmation) {
-        // Send the transaction
         const transaction = await tokenContract.transfer(recipientAddress, amountToSend);
         console.log("Transaction hash:", transaction.hash);
         await transaction.wait();
